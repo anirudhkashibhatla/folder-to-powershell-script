e735d0a76a631dfad416b2762454307f966717f5
Bharghava K <argav304@gmail.com>
2025-03-29 22:50:16 +0530

    initialize React app with Vite, add basic components and styles

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Convert you folder into powershell script files!</h1>
+        <button>Upload Folder/Files</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
